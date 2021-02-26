1d0c6a7aa9bd8cbf0b774073d025fc048ac06ae4
Paola Aguilar <pao.aquev29@gmail.com>
2021-02-25 19:46:25 -0600

    validation of code legnth equal to input length

--- konami/konami.js
+++ konami/konami.js
@@ -3,7 +3,7 @@ const code = "trainee";
 const codeLength = code.length;
 
 const keyUpHandler = (e) => {
-  console.log(e);
+  //   console.log(e);
   keyPressed = e.key;
   lowerKey = keyPressed.toLowerCase();
   keyPressedArray.push(lowerKey);
@@ -12,13 +12,19 @@ const keyUpHandler = (e) => {
     -codeLength - 1, //si no pongo el menos 1 diria que son 7 letras el tamano pero el splice toma indices entonces seria hasta el indice 6, de 0 a 6
     keyPressedArray.length - codeLength // va a eliminar lo que ingrese el usuario menos el lago del codigo (7)
   );
-  keyPressedString = keyPressedArray.join("");
-  console.log(keyPressedString);
-  if (keyPressedString.includes(code)) {
+  let keyPressedString = keyPressedArray.join("");
+  let keyPressedStringLength = keyPressedString.length;
+  if (codeLength === keyPressedStringLength && keyPressedString != code) {
+    location.reload();
+  } else if (
+    codeLength === keyPressedStringLength &&
+    keyPressedString === code
+  ) {
     console.log("DONE");
     konamiStatus("success");
   }
-  console.log(keyPressedArray);
+  console.log(keyPressedString);
+  //   console.log(keyPressedArray);
 };
 
 const konamiStatus = (status) => {
